d4729d9380f4e03070d07a9eae686e2bb312d1cc
iafominykh <iafominykh@yandex.ru>
2023-07-26 16:03:55 +0300

    CBV 1

--- catalog/models.py
+++ catalog/models.py
@@ -21,10 +21,10 @@ class Category(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=250, verbose_name='Наименование')
-    specification = models.TextField(verbose_name='Описание')
+    specification = models.TextField(**NULLABLE,verbose_name='Описание')
     preview_image = models.ImageField(**NULLABLE, upload_to='product/', verbose_name='Изображение')
     category = models.ForeignKey(Category, **NULLABLE, on_delete=models.CASCADE, verbose_name="категория")
-    price = models.IntegerField(verbose_name='Цена')
+    price = models.IntegerField(**NULLABLE, verbose_name='Цена')
     creation_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     last_modified = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
 
